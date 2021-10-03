cf23207b034a6967cd02bcdfca6ce14b94f2c562
Siarhei Abalonski <siarhei_abalonski@epam.com>
2021-10-03 17:40:02 +0300

    checkpoint task & mini store creator

--- tasks.js
+++ tasks.js
@@ -45,3 +45,68 @@ function get(obj, path, defaultValue = undefined) {
   })
   return result ? result : defaultValue
 }
+
+// (2).plus(3).minus(1) => 4.
+
+Number.prototype.plus = function(num) {
+  return this + num
+}
+Number.prototype.minus = function(num) {
+  return this - num
+}
+
+// createStore
+
+function createStore(reducer, initialState) {
+  let state = initialState
+  const subscribers = []
+  return {
+    dispatch: (action) => {
+      state = reducer(state, action)
+      subscribers.forEach(item => item(state))
+    },
+    getState: () => {
+      return state
+    },
+    subscribe : (subscriber) => {
+      subscribers.push(subscriber)
+    }
+  }
+}
+
+
+// subscribers, reducer and actions for testing
+// function mySubscriber1(state) {
+//   console.log('work', state);
+// }
+
+// function mySubscriber2(state) {
+//   console.log('work2', state);
+// }
+
+// const add_item = {
+//   type: 'ADD_ITEMS',
+//   payload: {
+//     a: 1,
+//     b: 2
+//   }
+// }
+
+// const add_item2 = {
+//   type: 'ADD_ITEMS2',
+//   payload: {
+//     a: 'a',
+//     b: 'b'
+//   }
+// }
+
+// function myReducer(state, action) {
+//   switch(action.type){
+//     case 'ADD_ITEMS': 
+//       return state = {...state,  some: action.payload}
+//     case 'ADD_ITEMS2':
+//       return state = {...state, some2: action.payload}
+//     default: 
+//       return state
+//   }
+// }
